11fa63c030d795971eb16a0743ff9e66aad72a57
Timo Selvaraj <timoselvaraj@Timos-MacBook-Pro.local>
2016-01-08 16:07:36 -0500

    Polly to Wally

--- crawl_download.py
+++ crawl_download.py
@@ -2,7 +2,7 @@ from pattern.web import Crawler
 from pattern.web import download
 from pattern.web import plaintext
 
-class Polly(Crawler):
+class Wally(Crawler):
     def visit(self, link, source=None):
         print 'visited:', repr(link.url), 'from:', link.referrer
         print plaintext(download(link.url))
@@ -10,6 +10,6 @@ class Polly(Crawler):
     def fail(self, link):
         print 'failed:', repr(link.url)
 
-p = Polly(links=['http://www.cnn.com/'], delay=1)
+p = Wally(links=['http://www.cnn.com/'], delay=1)
 while not p.done:
     p.crawl(method=2, cached=False, throttle=1)
--- crawler.py
+++ crawler.py
@@ -1,12 +1,12 @@
 from pattern.web import Crawler
 
-class Polly(Crawler):
+class Wally(Crawler):
     def visit(self, link, source=None):
         print 'visited:', repr(link.url), 'from:', link.referrer
 
     def fail(self, link):
         print 'failed:', repr(link.url)
 
-p = Polly(links=['http://www.cnn.com/'], delay=1)
+p = Wally(links=['http://www.cnn.com/'], delay=1)
 while not p.done:
     p.crawl(method=2, cached=False, throttle=1)
